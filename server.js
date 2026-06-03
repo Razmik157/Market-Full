f84c332e1066d049a643b1a7f3b8b00ecd1cdd5d
Razmik157 <mhertatev2007@gmail.com>
2026-06-03 11:52:58 +0400

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(cors());
 app.use(express.json({ limit: '2mb' }));
 app.use(express.urlencoded({ limit: '2mb', extended: true }));
-app.use('/uploads', express.static('uploads'));
+app.use('/uploads', express.static(UPLOADS_DIR));
 
 const DB_FILE = process.env.DB_FILE || 'db.json';
 
